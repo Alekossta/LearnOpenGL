b2336cad83c22232d102c9ecdcaef85f0bfbd58a
Alekossta <alekosstavrinoudakis@gmail.com>
2024-01-07 11:45:33 +0200

    made rectangle again but now with two textures

--- LearnOpenGL.cpp
+++ LearnOpenGL.cpp
@@ -54,13 +54,13 @@ int main()
     // every time our window changes size call our function
     glfwSetFramebufferSizeCallback(window, window_resize_callback);
 
-    // two triangles
-
+    // rectangle
     float vertices[] = {
-        // first triangle (left)
-        0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f, // bottom right red
-        -0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f, // bottom left green
-        0.0f, 0.5f, 0.0f, 0.0f, 0.0f, 1.0f // top blue
+        // position        // color          // texture cords
+        -0.5f, 0.5f, 0.0f, 1.0f, 0.0f, 0.0f, 0.0f, 1.0f,        // top left vertex
+        0.5f, 0.5f, 0.0f,  0.0f, 1.0f, 0.0f, 1.0f, 1.0f,        // top right vertex
+        -0.5f, -0.5f, 0.0f,0.0f, 0.0f, 1.0f, 0.0f, 0.0f,        // bottom left vertex
+        0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f, 0.0f,        // bottom right
     };
 
     unsigned VAO;
@@ -75,29 +75,74 @@ int main()
     // above we have defined our vertex data. the vertex data consists of many vertex attributes
 
     // position attribute
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 6, (void*)0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 8, (void*)0);
     glEnableVertexAttribArray(0); // enable the attribute at location = 0 (position of vertex)
 
     // color attribute
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 6, (void*)(3 * sizeof(float)));
-    glEnableVertexAttribArray(1); // enable the attribute at location = 0 (position of vertex)
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 8, (void*)(3 * sizeof(float)));
+    glEnableVertexAttribArray(1); // enable the attribute at location = 1 (color of vertex)
+
+    // texture coordinates attribute
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 8, (void*)(6 * sizeof(float)));
+    glEnableVertexAttribArray(2); // enable the attribute at location = 2 (texture coordinates of vertex)
+
+    int indices[] = {
+        0, 1, 2,
+        1, 2, 3
+    };
+
+    unsigned EBO; // element buffer object
+    glGenBuffers(1, &EBO);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
     glBindVertexArray(0);
 
     Shader defaultShader("shaders/vertex/default.vs", "shaders/fragment/default.fs");
 
-    unsigned texture;
-    glGenTextures(1, &texture); // generate a texture object
-    glBindTexture(GL_TEXTURE_2D, texture); // bind the texture
+    unsigned texture1;
+    glGenTextures(1, &texture1); // generate a texture object
+    glActiveTexture(GL_TEXTURE0);
+    glBindTexture(GL_TEXTURE_2D, texture1); // bind the texture
 
     // load the image
     int width, height, nrChannels;
     unsigned char* data = stbi_load("container.jpg", &width, &height, &nrChannels, 0);
 
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
-    glGenerateMipmap(GL_TEXTURE_2D);
-
+    if (data)
+    {
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+    }
+    else
+    {
+        std::cout << "Failed to load texture" << std::endl;
+    }
     stbi_image_free(data);
+
+    unsigned texture2;
+    glGenTextures(1, &texture2);
+    glActiveTexture(GL_TEXTURE1);
+    glBindTexture(GL_TEXTURE_2D, texture2);
+
+    // load the image
+    stbi_set_flip_vertically_on_load(true);
+    data = stbi_load("awesomeface.png", &width, &height, &nrChannels, 0);
+
+    if (data)
+    {
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+    }
+    else
+    {
+        std::cout << "Failed to load texture" << std::endl;
+    }
+    stbi_image_free(data);
+
+    defaultShader.use();
+    defaultShader.setInt("texture1", 0);
+    defaultShader.setInt("texture2", 1);
 
     while (!glfwWindowShouldClose(window))
     {
@@ -109,10 +154,8 @@ int main()
         glClear(GL_COLOR_BUFFER_BIT);
 
         // render
-        defaultShader.use();
-        defaultShader.setFloat("offsetRight", 0.25f);
         glBindVertexArray(VAO);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
         // after we are done making our new image/buffer swap our buffers
         glfwSwapBuffers(window);
