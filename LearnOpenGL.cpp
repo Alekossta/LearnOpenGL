f621f0a2cf7b951cf4063d54d8fd3de95410a59e
Alekossta <alekosstavrinoudakis@gmail.com>
2024-01-21 16:04:08 +0200

    seperated camera and shader implementation and definition

--- LearnOpenGL.cpp
+++ LearnOpenGL.cpp
@@ -150,18 +150,7 @@ int main()
         -0.5f,  0.5f, -0.5f
     };
 
-    glm::vec3 cubePositions[] = {
-        glm::vec3(0.0f,  0.0f,  0.0f),
-        glm::vec3(2.0f,  5.0f, -15.0f),
-        glm::vec3(-1.5f, -2.2f, -2.5f),
-        glm::vec3(-3.8f, -2.0f, -12.3f),
-        glm::vec3(2.4f, -0.4f, -3.5f),
-        glm::vec3(-1.7f,  3.0f, -7.5f),
-        glm::vec3(1.3f, -2.0f, -2.5f),
-        glm::vec3(1.5f,  2.0f, -2.5f),
-        glm::vec3(1.5f,  0.2f, -1.5f),
-        glm::vec3(-1.3f,  1.0f, -1.5f)
-    };
+    glm::vec3 cubePosition = glm::vec3(0.0f, 0.0f, 0.0f);
     unsigned int VBO, VAO;
     glGenVertexArrays(1, &VAO);
     glGenBuffers(1, &VBO);
@@ -198,7 +187,7 @@ int main()
         // render boxes
         glBindVertexArray(VAO);
         glm::mat4 model = glm::mat4(1.0f);
-        model = glm::translate(model, cubePositions[0]);
+        model = glm::translate(model, cubePosition);
         ourShader.setMat4("model", model);
 
         glDrawArrays(GL_TRIANGLES, 0, 36);
